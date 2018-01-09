577f69c6684d02bf78d5b25e522dad4f2ad49126
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-01-09 15:23:00 -0800

    wiring up routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  private
 
   def show_404
     render template: 'pages/error404.html.slim', status: :not_found
