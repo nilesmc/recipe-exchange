eb9312a36f8a8c3067ffdd4eb0079b5321f3822f
nilesmcgiver <zen.mind.pdx@gmail.com>
2018-01-09 15:12:27 -0800

    adding in pages controller and some basic controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  def show_404
+    render template: 'pages/error404.html.slim', status: :not_found
+  end
+
 end
